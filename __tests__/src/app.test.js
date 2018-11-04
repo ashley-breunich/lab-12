008cc9eb3ef871e93162d63327cc0ac394b579c6
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-11-03 21:13:08 -0700

    Continue working on test

--- __tests__/src/app.test.js
+++ __tests__/src/app.test.js
@@ -85,8 +85,23 @@ describe('api server', () => {
 
   });
 
+  it('should be able to post to /api/v1/products', ()  => {
+    
+    let obj = {category:'Sneakers', name:'Nike Sneakers', display_name:'Nike Sneakers', description:'These are shoes.'};
 
-  it('following a post, should find a single record', () => {
+    return mockRequest
+    .post('/api/v1/products')
+    .send(obj)
+    .then(results => {
+      expect(results.status).toBe(200);
+      expect(results.body.name).toEqual(obj.name);
+    })
+    .catch( err => console.error('err', err) );
+
+  });
+
+
+  it('following a post, should find a single record - categories', () => {
 
     let obj = {name:'Sandals', display_name:'Soludos Sandals', description:'These are shoes.'};
 
@@ -104,7 +119,25 @@ describe('api server', () => {
 
   });
 
-  it('following multiple posts, should return the correct count', () => {
+  it('following a post, should find a single record - products', () => {
+
+    let obj = {category:'Sneakers', name:'Nike Sneakers', display_name:'Nike Sneakers', description:'These are shoes.'};
+
+    return mockRequest
+    .post('/api/v1/products')
+    .send(obj)
+    .then(results => {
+      return mockRequest.get(`/api/v1/products/${results.body._id}`)
+      .then(list => {
+        expect(list.body[0].name).toEqual(obj.name);
+        expect(list.status).toBe(200);
+      })
+    })
+    .catch( err => console.error('err', err) );
+
+  });
+
+  it('following multiple posts, should return the correct count - categories', () => {
 
     return mockRequest
     .get('/api/v1/categories')
@@ -117,5 +150,73 @@ describe('api server', () => {
     });
 
   });
+
+});
+
+it('following multiple posts, should return the correct count - products', () => {
+
+  return mockRequest
+  .get('/api/v1/products')
+  .then(results => {
+    expect(results.body.count).toEqual(2);
+    expect(results.status).toBe(200);
+  })
+  .catch(err => {
+    expect(err).not.toBeDefined();
+  });
+
+});
+
+it('following a put, should update a single record', () => {
+
+  let obj = {name:'Pumps', display_name:'Manolo Blahnik Pumps', description:'These are shoes.', _id:10};
+  let objUpdated = {name:'Heels', display_name:'Manolo Blahnik Pumps', description:'These are shoes.',_id:10};
+
+  return mockRequest
+  .post('/api/v1/categories')
+  .send(obj)
+  .then(results => {
+    return mockRequest.put(`/api/v1/categories/10`)
+    .send(objUpdated)
+    .then(list => {
+      expect(list.body[0].name).toEqual(objUpdated.name);
+      expect(list.status).toBe(200);
+    })
+  })
+  .catch( err => console.error('err', err) );
+
+});
+
+it('following a delete, the record will be removed - categories', () => {
+
+  let obj = {name:'Sneakers', display_name:'Nike Sneakers', description:'These are shoes.'};
+
+  return mockRequest
+  .post('/api/v1/categories')
+  .send(obj)
+  .then(results => {
+    return mockRequest.delete(`/api/v1/categories/${results.body._id}`)
+    .then(list => {
+      expect(list.status).toBe(200);
+    })
+  })
+  .catch( err => console.error('err', err) );
+
+});
+
+it('following a delete, the record will be removed - products', () => {
+
+  let obj = {category:'Sneakers', name:'Nike Sneakers', display_name:'Nike Sneakers', description:'These are shoes.'};
+
+  return mockRequest
+  .post('/api/v1/products')
+  .send(obj)
+  .then(results => {
+    return mockRequest.delete(`/api/v1/products/${results.body._id}`)
+    .then(list => {
+      expect(list.status).toBe(200);
+    })
+  })
+  .catch( err => console.error('err', err) );
 
 });
