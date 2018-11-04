338f648de3993398f2174cd0b7eff3ee027f0221
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-11-03 17:10:14 -0700

    all models are working

--- src/models/mongo/products.js
+++ src/models/mongo/products.js
@@ -10,16 +10,16 @@ const products = mongoose.Schema({
   description: {type:String},
 });
 
-// categories.pre('validate', function() {
-//   console.log('Validating some stuff');
-// });
+products.pre('validate', function() {
+  console.log('Validating some stuff');
+});
 
-// categories.pre('save', function() {
-//   console.log('About to save some stuff', this);
-// });
+products.pre('save', function() {
+  console.log('About to save some stuff', this);
+});
 
-// categories.post('save', function() {
-//   console.log('Saved', this);
-// });
+products.post('save', function() {
+  console.log('Saved', this);
+});
 
 export default mongoose.model('products', products);
