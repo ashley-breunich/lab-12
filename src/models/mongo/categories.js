da8bfeac3990fb99d5e0088a93c0687abe373acc
ASHLEY BREUNICH <ashleybreunich@gmail.com>
2018-11-03 18:05:52 -0700

    Got Virtual Populating Working

--- src/models/mongo/categories.js
+++ src/models/mongo/categories.js
@@ -7,6 +7,23 @@ const categories = mongoose.Schema({
   name: {type:String, require: true},
   display_name: {type:String, require: true},
   description: {type:String},
+}, {toObject:{virtuals:true}, toJSON:{virtuals:true}});
+
+categories.virtual('products', {
+  ref: 'products',
+  localField: 'name',
+  foreignField: 'category',
+  justOne: false,
+});
+
+categories.pre('find', function() {
+  try {
+    console.log('joining...');
+    this.populate('products');
+  }
+  catch(e) {
+    console.error(e);
+  }
 });
 
 categories.pre('validate', function() {
